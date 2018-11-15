87f2fe22f44534e7a21cdd1d026b09e9adbc2fb4
Corey Henderson <corman@cormander.com>
2018-11-14 17:52:19 -0700

    don't overwrite existing keys for safety

--- utils/genkeys.js
+++ utils/genkeys.js
@@ -2,6 +2,11 @@
 var fs = require('fs');
 var keypair = require('keypair');
 
+if (fs.existsSync("rsa.key")) {
+  console.error("ERROR: rsa.key exists, not overwriting. Exiting!");
+  process.exit(1);
+}
+
 console.log("Generating keypair for sm-oauth");
 var pair = keypair();
 
